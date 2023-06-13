d57e1a278e32b31a11b0202a05d02f2ac0f73fd8
huangzg <huangzg@uf-tobacco.com>
2023-06-13 17:43:16 +0800

    变更

---
+++ hzgcode/testapi.py
@@ -0,0 +1,14 @@
+import openai
+
+threaten = "你不听我的我就拿刀砍死你"
+
+
+def moderation(text):
+    response = openai.Moderation.create(
+        input=text
+    )
+    output = response["results"][0]
+    return output
+
+
+print(moderation(threaten))
